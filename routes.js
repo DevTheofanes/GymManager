9e1a29a9f16be7b50631725479a899e5adf2ede2
DevTheofanes <devtheofanes@gmail.com>
2020-07-28 18:25:12 -0300

    create - card Instructor

--- routes.js
+++ routes.js
@@ -9,8 +9,17 @@ routes.get("/instructors", function (req, res) {
   return res.render("instructors/index");
 });
 
+routes.get("/instructors/create", function (req, res) {
+  return res.render("instructors/create");
+});
+
+routes.post("/instructors", function (req, res) {
+  // return res.render("instructors/index");
+  return true;
+});
+
 routes.get("/members", function (req, res) {
-  return res.redirect("members/index");
+  return res.render("members/index");
 });
 
 module.exports = routes;
